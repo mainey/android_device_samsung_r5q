fddc43f2c48a8ea6a6919f8d1bb3839c8dad2539
Michael Benedict <michaelbt@live.com>
2020-03-15 19:30:22 +1100

    initial commit
    Signed-off-by: Michael Benedict <michaelbt@live.com>

---
+++ device.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/samsung/r5q
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+PRODUCT_TARGET_VNDK_VERSION := 29
+PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
+# No A/B
+AB_OTA_UPDATER := false
+
+# Audio 
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext \
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
