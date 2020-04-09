a2104a98119f83235875c467e6b33f10df5c5335
Michael Benedict <michaelbt@live.com>
2020-04-09 17:27:00 +1000

    r5q: start using vendor_overlay
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- config.fs
+++ config.fs
@@ -57,3 +57,15 @@ mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
+
+[system/product/vendor_overlay/*/bin/*]
+mode: 0755
+user: AID_ROOT
+group: AID_SHELL
+caps: 0
+
+[product/vendor_overlay/*/bin/*]
+mode: 0755
+user: AID_ROOT
+group: AID_SHELL
+caps: 0
