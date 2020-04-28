7d0fa5b4e20507828193b65c6d2106e23f3e0637
Michael Benedict <michaelbt@live.com>
2020-04-29 01:19:35 +1000

    r5q: import ramdisks
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- device.mk
+++ device.mk
@@ -128,7 +128,33 @@ PRODUCT_COPY_FILES += \
 
 # Init
 PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.crashdata.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.fm.sh \
+    init.qti.ims.sh \
+    qca6234-service.sh \
+    init.msm.usb.configfs.rc \
+    init.qcom.factory.rc \
     init.qcom.rc \
+    init.qcom.usb.rc \
+    init.r5q.rc \
+    init.samsung.bsp.rc \
+    init.samsung.display.rc \
+    init.samsung.rc \
+    init.target.rc \
+    fstab.qcom \
+    ueventd.qcom.rc \
     loggy.sh
 
 # IRQ
