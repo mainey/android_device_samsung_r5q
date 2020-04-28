d0034e0a8c0bcc569853ff5835f2c91a75e16822
Ataberk Özen <ataberkozen123@gmail.com>
2020-04-28 20:17:33 +0300

    r5q: build qti-telephony-hidl-wrapper and qti-telephony-utils
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- device.mk
+++ device.mk
@@ -374,6 +374,10 @@ PRODUCT_COPY_FILES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
