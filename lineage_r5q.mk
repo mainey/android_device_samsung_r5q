d464e6b7e44df3160dc7c30fc04ba913a9886eb3
Michael Benedict <michaelbt@live.com>
2020-11-25 01:01:01 +0700

    r5q: initial commit
    Signed-off-by: Michael Benedict <michaelbt@live.com>

---
+++ lineage_r5q.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from r5q device
+$(call inherit-product, device/samsung/r5q/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_r5q
+PRODUCT_DEVICE := r5q
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G770F
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Use the latest approved GMS identifiers
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=r5q \
+    TARGET_BOOTLOADER_BOARD_NAME=msmnile \
+    PRODUCT_NAME=r5qnaxx \
+    PRIVATE_BUILD_DESC="r5qnaxx-user 10 QP1A.190711.020 G770FXXS3CTJ3 release-keys"
+
+BUILD_FINGERPRINT := "samsung/r5qnaxx/r5q:10/QP1A.190711.020/G770FXXS3CTJ3:user/release-keys"
