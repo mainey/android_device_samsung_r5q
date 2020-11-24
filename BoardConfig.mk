c2edf6d38f5eaabe1eb555e8cd5129a3e31749fe
Michael Benedict <michaelbt@live.com>
2020-11-25 01:01:01 +0700

    r5q: initial commit
    Signed-off-by: Michael Benedict <michaelbt@live.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,67 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit common board flags
+include device/samsung/sm8150-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/r5q
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 2
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048 androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x02000000
+BOARD_KERNEL_SECOND_OFFSET := 0x00f00000
+BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
+BOARD_DTB_OFFSET := 0x01f00000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --dtb_offset $(BOARD_DTB_OFFSET) 
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --second_offset $(BOARD_KERNEL_SECOND_OFFSET) --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE) --board "SRPSG08A002"
+TARGET_KERNEL_SOURCE := kernel/samsung/r5q
+TARGET_KERNEL_CONFIG := r5q_eur_open_defconfig
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864 
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 82726912 
+BOARD_DTBOIMG_PARTITION_SIZE := 33554432
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 114934394880
+BOARD_CACHEIMAGE_PARTITION_SIZE := 629145600
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 614572800
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 3758096384
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+
+BOARD_SUPER_PARTITION_SIZE := 8053063680 
+BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions 
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 8048869376
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := \
+    system \
+    product
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
